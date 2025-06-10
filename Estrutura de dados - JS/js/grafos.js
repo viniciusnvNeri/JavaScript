fc52c1a9484dab07857bf2b6602d51a2649dc87b
viniciusnvNeri <119890809+viniciusnvNeri@users.noreply.github.com>
2025-06-10 16:17:27 -0300

    Update grafos.js
    Atualização nos comentários do código

--- Estrutura de dados - JS/js/grafos.js
+++ Estrutura de dados - JS/js/grafos.js
@@ -1,199 +1,211 @@
+// Estrutura de dicionário (chave-valor), usada para guardar os vértices e suas conexões
 function Dictionary() {
-  var items = {}
+    var items = {};
 
-  this.set = function(key, value) {
-    //adiciona um novo item ao dicionário
-    items[key] = value
-  }
-
-  this.delete = function(key) {
-    //remove um item do dicionário
-    if(this.has(key)) {
-        delete items[key]
-        return true
+    this.set = function(key, value) {
+        // Adiciona item ao dicionário
+        items[key] = value;
     }
-    return false
-  }
 
-  this.get = function(key) {
-    //devolve um valor especifico a partir da chave
-    return this.has(key) ? items[key] : undefined
-  }
-
-  this.has = function(key) {
-    //verifica se a chave existe e retorna um booleano
-    return items.hasOwnProperty(key)
-  }
-
-  this.clear = function() {
-    //remove todos os itens do dicionário
-    items = {}
-  }
-
-  this.size = function() {
-    //retorna a quantidade de elementos contidos no dicionário
-    return Object.keys(items).length
-  }
-
-  this.keys = function() {
-    //devolve um array com todas as chaves do dicionário
-    return Object.keys(items)
-  }
-
-  this.values = function() {
-    //devolve um array com todos os valores do dicionário
-    var values =[],
-    keys = Object.keys(items)
-
-    for(var i = 0; i < keys.length; i++) {
-      values.push(items[keys[i]])
+    this.delete = function(key) {
+        // Remove item, se existir
+        if (this.has(key)) {
+            delete items[key];
+            return true;
+        }
+        return false;
     }
-    return values
-  }
+
+    this.get = function(key) {
+        // Retorna o valor associado à chave
+        return this.has(key) ? items[key] : undefined;
+    }
+
+    this.has = function(key) {
+        // Verifica se a chave existe
+        return items.hasOwnProperty(key);
+    }
+
+    this.clear = function() {
+        items = {};
+    }
+
+    this.size = function() {
+        return Object.keys(items).length;
+    }
+
+    this.keys = function() {
+        return Object.keys(items);
+    }
+
+    this.values = function() {
+        var values = [],
+            keys = Object.keys(items);
+
+        for (var i = 0; i < keys.length; i++) {
+            values.push(items[keys[i]]);
+        }
+        return values;
+    }
 
     this.getItems = function() {
-        //devolve todos os itens do dicionário
-        return items
+        return items;
     }
 }
 
+// Estrutura de fila (queue), usada em BFS
 function Queue() {
-  var items = []
+    var items = [];
 
-  this.enqueue = function(element) {
-    //Adiciona um elemento ao final da fila
-    items.push(element);
-  }
-
-  this.dequeue = function() {
-    //Remove o primeiro elemento da fila
-    return items.shift();
-  }
-
-  this.front = function() {
-    //Retorna o primeiro elemento da fila sem removê-lo
-    return items[0];
-  }
-
-  this.isEmpty = function() {
-    //Verifica se a fila está vazia
-    return items.length === 0;
-  }
-
-  this.size = function() {
-    //Retorna o tamanho da fila
-    return items.length;
-  }
-
-  this.print = function() {
-    //Imprime os elementos da fila
-    console.log(items.toString());
-  }
-} 
-
-function Graph(){
-    var vertices = []
-    var adjList = new Dictionary()
-
-    this.addVertex = function(v){
-        vertices.push(v)
-        adjList.set(v, [])
+    this.enqueue = function(element) {
+        items.push(element);
     }
 
-    this.addEdge = function(v, w){
-        adjList.get(v).push(w)
-        adjList.get(w).push(v)
+    this.dequeue = function() {
+        return items.shift();
     }
 
-    this.toString = function(){
-       var s = ''
-       for(var i = 0; i < vertices.length; i++){
-           s += vertices[i] + ' -> '
-           var neighbors = adjList.get(vertices[i])
-           for(var j = 0; j < neighbors.length; j++){
-               s += neighbors[j] + ' '
-           }
-           s += '\n'
-       }
-       return s
+    this.front = function() {
+        return items[0];
     }
 
-    var initializeColor = function(){
-        var color = []
-        for(var i = 0; i < vertices.length; i++){
-            color[vertices[i]] = 'white'
+    this.isEmpty = function() {
+        return items.length === 0;
+    }
+
+    this.size = function() {
+        return items.length;
+    }
+
+    this.print = function() {
+        console.log(items.toString());
+    }
+}
+
+// Estrutura de Grafo com lista de adjacência
+function Graph() {
+    var vertices = [];            // Lista de vértices
+    var adjList = new Dictionary(); // Lista de adjacência
+
+    this.addVertex = function(v) {
+        // Adiciona um novo vértice
+        vertices.push(v);
+        adjList.set(v, []);
+    }
+
+    this.addEdge = function(v, w) {
+        // Adiciona uma aresta entre v e w (grafo não direcionado)
+        adjList.get(v).push(w);
+        adjList.get(w).push(v);
+    }
+
+    this.toString = function() {
+        // Representação do grafo como string
+        var s = '';
+        for (var i = 0; i < vertices.length; i++) {
+            s += vertices[i] + ' -> ';
+            var neighbors = adjList.get(vertices[i]);
+            for (var j = 0; j < neighbors.length; j++) {
+                s += neighbors[j] + ' ';
+            }
+            s += '\n';
         }
-        return color
+        return s;
     }
 
-    this.bfs = function(v, callback){
-        var color = initializeColor(),
-        queue = new Queue();
-        queue.enqueue(v)
+    var initializeColor = function() {
+        // Inicializa as cores dos vértices para 'white' (não visitado)
+        var color = [];
+        for (var i = 0; i < vertices.length; i++) {
+            color[vertices[i]] = 'white';
+        }
+        return color;
+    }
 
-        while(!queue.isEmpty()){
-            var u = queue.dequeue()
-            var neighbors = adjList.get(u)
-            color[u] = 'grey'
-            for(var i = 0; i < neighbors.length; i++){
-                var w = neighbors[i]
-                if(color[w] === 'white'){
-                    color[w] = 'grey'
-                    queue.enqueue(w)
+    this.bfs = function(v, callback) {
+        // Busca em Largura (BFS)
+        var color = initializeColor(),
+            queue = new Queue();
+        queue.enqueue(v);
+
+        while (!queue.isEmpty()) {
+            var u = queue.dequeue();
+            var neighbors = adjList.get(u);
+            color[u] = 'grey'; // visitando
+
+            for (var i = 0; i < neighbors.length; i++) {
+                var w = neighbors[i];
+                if (color[w] === 'white') {
+                    color[w] = 'grey';
+                    queue.enqueue(w);
                 }
             }
-            color[u] = 'black'
-            callback(u)
-            
+
+            color[u] = 'black'; // totalmente visitado
+            callback(u);
         }
     }
 
     this.dfs = function(callback) {
-        var color = initializeColor()
-        for(var i = 0; i < vertices.length; i++){
-            if(color[vertices[i]] === 'white'){
-                dfsVisit(vertices[i], color, callback)
+        // Busca em Profundidade (DFS)
+        var color = initializeColor();
+        for (var i = 0; i < vertices.length; i++) {
+            if (color[vertices[i]] === 'white') {
+                dfsVisit(vertices[i], color, callback);
             }
         }
     }
 
     var dfsVisit = function(u, color, callback) {
-        color[u] = 'grey'
-        callback(u)
-        var neighbors = adjList.get(u)
-        for(var i = 0; i < neighbors.length; i++){
-            var w = neighbors[i]
-            if(color[w] === 'white'){
-                dfsVisit(w, color, callback)
+        // Função recursiva auxiliar da DFS
+        color[u] = 'grey';
+        callback(u);
+
+        var neighbors = adjList.get(u);
+        for (var i = 0; i < neighbors.length; i++) {
+            var w = neighbors[i];
+            if (color[w] === 'white') {
+                dfsVisit(w, color, callback);
             }
         }
-        color[u] = 'black'
+
+        color[u] = 'black';
     }
 }
 
+// Função usada nos percursos para imprimir os vértices
 function printNode(value) {
-    console.log('Visited vertex: ' + value)
+    console.log('Visitado: ' + value);
 }
 
-var graph = new Graph()
-var myVertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
-for(var i = 0; i < myVertices.length; i++){
-    graph.addVertex(myVertices[i])
+// ---------------------------
+// Exemplo de uso do grafo
+// ---------------------------
+
+var graph = new Graph();
+var myVertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+
+for (var i = 0; i < myVertices.length; i++) {
+    graph.addVertex(myVertices[i]);
 }
 
-graph.addEdge('A', 'B')
-graph.addEdge('A', 'C')
-graph.addEdge('A', 'D')
-graph.addEdge('C', 'D')
-graph.addEdge('C', 'G')
-graph.addEdge('D', 'G')
-graph.addEdge('D', 'H')
-graph.addEdge('B', 'E')
-graph.addEdge('B', 'F')
-graph.addEdge('E', 'I')
+// Adiciona as conexões (arestas)
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('A', 'D');
+graph.addEdge('C', 'D');
+graph.addEdge('C', 'G');
+graph.addEdge('D', 'G');
+graph.addEdge('D', 'H');
+graph.addEdge('B', 'E');
+graph.addEdge('B', 'F');
+graph.addEdge('E', 'I');
 
+// Execução do percurso em profundidade (DFS)
+graph.dfs(printNode);
 
+// Para usar BFS, descomente a linha abaixo:
+// graph.bfs(myVertices[0], printNode);
 
-// graph.bfs(myVertices[0], printNode)
-graph.dfs(printNode)
-// console.log(graph.toString())
+// Para ver o grafo como string, descomente a linha abaixo:
+// console.log(graph.toString());
